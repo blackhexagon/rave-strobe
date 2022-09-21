ce315675cbee3b7c78a6cde63c9295245314913b
Matous Trca <matous.trca@gmail.com>
2022-09-21 20:59:06 +0200

    do not use canvas

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,26 @@
 import AudioMotionAnalyzer from "audiomotion-analyzer"
 
-async function getAudio(): Promise<AudioMotionAnalyzer|null> {
-    try {
-        const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-        const analyzer = new AudioMotionAnalyzer()
-        const micStream = analyzer.audioCtx.createMediaStreamSource( stream );
-        analyzer.connectInput( micStream );
-        // mute output to prevent feedback loops from the speakers
-        analyzer.volume = 0;
-        return analyzer
-    } catch (e) {
-        console.error("cannot connect to the microphone")
-        return null
-    }
+async function start(): Promise<AudioMotionAnalyzer> {
+    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+    const analyzer = new AudioMotionAnalyzer(null, {
+        useCanvas: false,
+        connectSpeakers: false,
+        onCanvasDraw(instance) {
+            const bassEnergy = instance.getEnergy("bass")
+            console.log(bassEnergy)
+        }
+    })
+    const micStream = analyzer.audioCtx.createMediaStreamSource( stream );
+    analyzer.connectInput( micStream );
+    // mute output to prevent feedback loops from the speakers
+    analyzer.volume = 0;
+    return analyzer
 }
 
-getAudio()
-    .then((analyzer: AudioMotionAnalyzer) => {
-        console.log({analyzer})
+start()
+    .then(() => {
+        console.log("rave strobe is running")
+    })
+    .catch((e) => {
+        console.error(e)
     })

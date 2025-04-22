08d20aaa24eb1e047e1fb4c9f2e9291b56937590
matous <matous.trca@gmail.com>
2025-04-22 12:00:04 +0200

    Add eslint config and showcase module files

---
+++ packages/showcase/main.js
@@ -0,0 +1,21 @@
+import "./style.css";
+
+const screens = document.querySelectorAll("section");
+let index = 0;
+
+function update(index) {
+  screens.forEach((screen, i) => {
+    if (i === index) {
+      screen.classList.add("active");
+    } else {
+      screen.classList.remove("active");
+    }
+  });
+}
+setInterval(() => {
+  index++;
+  if (index >= screens.length) {
+    index = 0;
+  }
+  update(index);
+}, 1000);
